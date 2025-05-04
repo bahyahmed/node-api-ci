893c66b262bee9f1f0577d5a45334c7a565dc4a4
bahyahmed <biboacc0101@gmail.com>
2025-05-04 12:08:26 +0300

    fix:added ESlint config for v9

---
+++ eslint.config.js
@@ -0,0 +1,14 @@
+export default [
+    {
+      files: ['src/**/*.js'],
+      languageOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      rules: {
+        semi: ['error', 'always'],
+        'no-unused-vars': 'warn',
+      },
+    },
+  ];
+  
